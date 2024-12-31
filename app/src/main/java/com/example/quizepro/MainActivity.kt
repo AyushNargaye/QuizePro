ed6d4bc39553ebcd9aef155d0d818d846775022b
AyushNargaye <ayushnargaye@gmail.com>
2024-12-31 13:37:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizepro/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.quizepro
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+        val etName : EditText = findViewById(R.id.etName)
+        val btnStart : Button = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name First!", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this,QuizeQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+}
